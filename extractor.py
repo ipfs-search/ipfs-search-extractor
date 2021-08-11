841a378e4ae4491e42f45e1e896f866be35883d0
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-08-11 12:15:34 +0100

    Add start and end date, in theory we now have incremental support.

--- extractor.py
+++ extractor.py
@@ -1,8 +1,15 @@
 import elasticsearch
 from elasticsearch.helpers import scan
 import json
+import sys
+import time
 
-es = elasticsearch.Elasticsearch('http://10.200.200.3:9200')
+ELASTICSEARCH_URL = 'http://10.200.200.3:9200'
+MAX_DOCS = 1024*1024
+START_DATE = "2018-01-01T00:00:00Z"
+END_DATE = "2019-01-01T00:00:00Z"
+
+es = elasticsearch.Elasticsearch(ELASTICSEARCH_URL)
 es_response = scan(
     es,
     index='ipfs_files_v8',
@@ -19,16 +26,38 @@ es_response = scan(
                         "wildcard": {
                             "metadata.title": "?*" # Ensure not empty
                         }
+                    },
+                    {
+                        "range": {
+                            "first-seen": {
+                                "gt": START_DATE,
+                                "lte": END_DATE
+                            }
+                        }
                     }
                 ]
-            }
+            },
         },
+        "sort":[
+            {
+                "first-seen": { # Allows for incremental exports
+                    "order": "asc"
+                }
+            }
+        ],
         "_source": [
             "metadata.title",
+            "first-seen",
             "references"
         ]
-    }
+    },
+    preserve_order=True
 )
+
+doc_count = 0
+start_time = time.time()
+first_item = None
+last_item = None
 
 for item in es_response:
     _source = item['_source']
@@ -42,6 +71,21 @@ for item in es_response:
         parent_hash = None
     else:
         parent_hash = references[0]['parent_hash']
-    # print('\t'.join([item['_id'], title, parent_hash]))
-    # it seems parent_hash is not so useful for me now, you could also export the json like below🔽 if you like(I will analyse the sites via title):
-    print(json.dumps([item['_id'], title, parent_hash]))
+
+    assert _source['first-seen']
+    first_seen = _source['first-seen']
+    print(json.dumps([item['_id'], title, first_seen, parent_hash]))
+
+    doc_count += 1
+
+    if doc_count == 1:
+        first_item = first_seen
+
+    if doc_count == MAX_DOCS:
+        break
+
+last_item = first_seen
+
+time_spent = time.time() - start_time
+print("{0} documents written in {1}".format(doc_count, time_spent), file=sys.stderr)
+print("First item: {0}\nLast item: {1}".format(first_item, last_item), file=sys.stderr)
